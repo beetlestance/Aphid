ebf1e2acce86ae915f3430f0710763634e74fe8a
akshay <akshay253101yadav@gmail.com>
2020-08-30 13:19:33 +0530

    wildcard imports removed copyright added to all files

--- mdc-theme-lint/src/main/java/com/beetlestance/mdc_theme_lint/detectors/DirectColorDrawableUseDetector.kt
+++ mdc-theme-lint/src/main/java/com/beetlestance/mdc_theme_lint/detectors/DirectColorDrawableUseDetector.kt
@@ -1,3 +1,18 @@
+/*
+ * Copyright 2020 BeetleStance
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 package com.beetlestance.mdc_theme_lint.detectors
 
 import com.android.resources.ResourceFolderType
@@ -6,7 +21,7 @@ import com.android.tools.lint.detector.api.XmlContext
 import com.beetlestance.mdc_theme_lint.issues.DirectColorDrawableUseIssue
 import org.w3c.dom.Attr
 
-//Vector Drawables.
+// Vector Drawables.
 private const val FILL_COLOR = "fillColor"
 private const val STROKE_COLOR = "strokeColor"
 private const val COLOR = "color"
--- mdc-theme-lint/src/main/java/com/beetlestance/mdc_theme_lint/issues/DirectColorUseIssue.kt
+++ mdc-theme-lint/src/main/java/com/beetlestance/mdc_theme_lint/issues/DirectColorUseIssue.kt
@@ -1,6 +1,25 @@
+/*
+ * Copyright 2020 BeetleStance
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 package com.beetlestance.mdc_theme_lint.issues
 
-import com.android.tools.lint.detector.api.*
+import com.android.tools.lint.detector.api.Category
+import com.android.tools.lint.detector.api.Implementation
+import com.android.tools.lint.detector.api.Issue
+import com.android.tools.lint.detector.api.Scope
+import com.android.tools.lint.detector.api.Severity
 import com.beetlestance.mdc_theme_lint.detectors.DirectColorUseDetector
 
 object DirectColorUseIssue {
