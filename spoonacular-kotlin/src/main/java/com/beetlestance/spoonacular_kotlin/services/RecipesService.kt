f67aa46c35daf1a28f09d9c27cba797d1179729a
akshay <akshay253101yadav@gmail.com>
2020-10-04 23:19:50 +0530

    Spotless Apply
    updated license header file

--- spoonacular-kotlin/src/main/java/com/beetlestance/spoonacular_kotlin/services/RecipesService.kt
+++ spoonacular-kotlin/src/main/java/com/beetlestance/spoonacular_kotlin/services/RecipesService.kt
@@ -13,7 +13,6 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-
 package com.beetlestance.spoonacular_kotlin.services
 
 import com.beetlestance.spoonacular_kotlin.models.request.recipe.RequestAnalyzeRecipeInstructions
--- spoonacular-kotlin/src/main/java/com/beetlestance/spoonacular_kotlin/services/endpoints/MealPlanner.kt
+++ spoonacular-kotlin/src/main/java/com/beetlestance/spoonacular_kotlin/services/endpoints/MealPlanner.kt
@@ -1,3 +1,18 @@
+/*
+ * Copyright 2020 BeetleStance
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 package com.beetlestance.spoonacular_kotlin.services.endpoints
 
 internal object MealPlanner {
@@ -9,7 +24,6 @@ internal object MealPlanner {
             const val ADD_TO_MEAL_PLAN = "/mealplanner/{username}/items"
 
             const val DELETE_FROM_MEAL_PLAN = "/mealplanner/{username}/items/{id}"
-
         }
 
         object Templates {
@@ -17,7 +31,6 @@ internal object MealPlanner {
             const val GET_MEAL_PLAN_TEMPLATE = "/mealplanner/{username}/templates/{id}"
 
             const val GET_MEAL_PLAN_TEMPLATES = "/mealplanner/{username}/templates"
-
         }
 
         object ShoppingLists {
@@ -30,12 +43,10 @@ internal object MealPlanner {
                 "/mealplanner/{username}/shopping-list/{start-date}/{end-date}"
 
             const val GET_SHOPPING_LIST = "/mealplanner/{username}/shopping-list"
-
         }
 
         const val GET_MEAL_PLAN_WEEK = "/mealplanner/{username}/week/{start-date}"
     }
 
     const val GENERATE_MEAL_PLAN = "/mealplanner/generate"
-
 }
