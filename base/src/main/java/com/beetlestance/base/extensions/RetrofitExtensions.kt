c88d738a75e0d69c7ca9588aeaf25cb9b7ec71c4
kameshyadavky <kameshyadavky@gmail.com>
2020-10-06 15:12:40 +0530

    Wildcard imports removed

--- base/src/main/java/com/beetlestance/base/extensions/RetrofitExtensions.kt
+++ base/src/main/java/com/beetlestance/base/extensions/RetrofitExtensions.kt
@@ -1,9 +1,24 @@
+/*
+ * Copyright 2020 BeetleStance
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 package com.beetlestance.base.extensions
 
 import com.beetlestance.base.result.Failure
 import com.beetlestance.base.result.Result
 import com.beetlestance.base.result.Success
-import com.beetlestance.spoonacular_kotlin.models.*
+import com.beetlestance.spoonacular_kotlin.models.SpoonacularApiResponse
 import kotlinx.coroutines.delay
 import kotlinx.coroutines.suspendCancellableCoroutine
 import retrofit2.Call
@@ -101,4 +116,3 @@ fun <T> SpoonacularApiResponse<T>.toResult(): Result<T> = try {
 } catch (e: Exception) {
     Failure(e)
 }
-
