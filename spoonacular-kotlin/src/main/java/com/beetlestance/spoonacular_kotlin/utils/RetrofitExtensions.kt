9a282dc9c74ceeea0f583fa267648be5b495d0be
akshay <akshay253101yadav@gmail.com>
2020-10-09 10:52:12 +0530

    Spoonacular Kotlin Retrofit Extensions
    nullable type removed

--- spoonacular-kotlin/src/main/java/com/beetlestance/spoonacular_kotlin/utils/RetrofitExtensions.kt
+++ spoonacular-kotlin/src/main/java/com/beetlestance/spoonacular_kotlin/utils/RetrofitExtensions.kt
@@ -3,6 +3,6 @@ package com.beetlestance.spoonacular_kotlin.utils
 import com.beetlestance.spoonacular_kotlin.models.SpoonacularApiResponse
 import retrofit2.Response
 
-inline fun <reified T> Response<T>.toSpoonacularApiResponse(): SpoonacularApiResponse<T?>? {
+inline fun <reified T> Response<T>.toSpoonacularApiResponse(): SpoonacularApiResponse<T> {
     return raw().toSpoonacularApiResponse()
 }
