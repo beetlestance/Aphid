4465d77ac7407fe855f94b7d2662de415afebbf2
akshay <akshay253101yadav@gmail.com>
2020-10-14 11:06:37 +0530

    Compose-Explore
    Quick Recipes and Recently Viewed Content Added

--- app/src/main/java/com/beetlestance/aphid/Explore.kt
+++ app/src/main/java/com/beetlestance/aphid/Explore.kt
@@ -1,23 +1,16 @@
 package com.beetlestance.aphid
 
 import androidx.compose.foundation.BorderStroke
-import androidx.compose.foundation.Icon
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.ScrollableColumn
 import androidx.compose.foundation.ScrollableRow
 import androidx.compose.foundation.Text
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
-import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.ConstraintLayout
 import androidx.compose.foundation.layout.Dimension
-import androidx.compose.foundation.layout.ExperimentalLayout
-import androidx.compose.foundation.layout.FlowCrossAxisAlignment
-import androidx.compose.foundation.layout.FlowMainAxisAlignment
-import androidx.compose.foundation.layout.FlowRow
 import androidx.compose.foundation.layout.PaddingValues
-import androidx.compose.foundation.layout.SizeMode
 import androidx.compose.foundation.layout.aspectRatio
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
@@ -26,10 +19,7 @@ import androidx.compose.foundation.layout.preferredWidth
 import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.Card
-import androidx.compose.material.EmphasisAmbient
-import androidx.compose.material.IconToggleButton
 import androidx.compose.material.MaterialTheme
-import androidx.compose.material.ProvideEmphasis
 import androidx.compose.material.Surface
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
@@ -54,21 +44,25 @@ fun Explore() {
             modifier = Modifier.fillMaxSize(),
             contentPadding = PaddingValues(16.dp),
             verticalArrangement = Arrangement.spacedBy(
-                space = 16.dp,
+                space = 24.dp,
                 alignment = Alignment.Top
             )
         ) {
-            BreakFast()
+            BreakFastWithHeader()
 
             MoodContent()
 
             Cuisine()
+
+            QuickRecipesWithHeader()
+
+            RecentlyViewedRecipesWithHeader()
         }
     }
 }
 
 @Composable
-fun BreakFast() {
+fun BreakFastWithHeader() {
 
     Text(
         text = stringResource(id = R.string.explore_breaksfast_header),
@@ -83,93 +77,15 @@ fun BreakFast() {
     ) {
 
         repeat(4) {
-            BreakFastContent()
-        }
-    }
-}
-
-@Composable
-fun BreakFastContent() {
-    val itemWidth = (ConfigurationAmbient.current.screenWidthDp * 0.7f).dp
-    Column(
-        modifier = Modifier.preferredWidth(itemWidth).fillMaxWidth(),
-        verticalArrangement = Arrangement.spacedBy(
-            space = 4.dp,
-            alignment = Alignment.Top
-        )
-    ) {
-
-        BreakFastCard()
-
-        BreakFastDetails()
-    }
-}
-
-@Composable
-fun BreakFastCard() {
-    Card(shape = RoundedCornerShape(16.dp)) {
-        Box {
-            Image(
-                modifier = Modifier.fillMaxWidth(),
-                asset = imageResource(id = R.drawable.temp_brownie),
-                contentScale = ContentScale.Crop
+            FoodCardWithDetails(
+                fraction = 0.7f,
+                imageResource = R.drawable.temp_brownie,
+                contentTags = "2 Serving • 40 Min • 331 Cal ",
+                rating = "4.3",
+                name = "Creamy Donut",
             )
-
-            ProvideEmphasis(emphasis = EmphasisAmbient.current.medium) {
-                IconToggleButton(
-                    modifier = Modifier
-                        .align(Alignment.TopEnd)
-                        .padding(16.dp)
-                        .background(
-                            shape = CircleShape,
-                            color = colorResource(id = R.color.grey_400_alpha_30)
-                        ),
-                    icon = {
-                        Icon(
-                            asset = vectorResource(id = R.drawable.ic_like),
-                            tint = colorResource(R.color.deep_orange_a200)
-                        )
-                    },
-                    checked = false,
-                    onCheckedChange = {}
-                )
-            }
         }
     }
-}
-
-@OptIn(ExperimentalLayout::class)
-@Composable
-fun BreakFastDetails() {
-    FlowRow(
-        mainAxisAlignment = FlowMainAxisAlignment.SpaceBetween,
-        mainAxisSize = SizeMode.Expand,
-        crossAxisAlignment = FlowCrossAxisAlignment.Center,
-        crossAxisSpacing = 4.dp
-    ) {
-        Text(
-            text = "2 Serving • 40 Min • 331 Cal ",
-            style = MaterialTheme.typography.body2,
-            color = colorResource(id = R.color.grey_700)
-        )
-
-        Text(
-            modifier = Modifier
-                .background(
-                    color = colorResource(id = R.color.deep_orange_a200),
-                    shape = RoundedCornerShape(10.dp)
-                )
-                .padding(horizontal = 12.dp, vertical = 2.dp),
-            text = "4.3",
-            style = MaterialTheme.typography.subtitle2,
-            color = MaterialTheme.colors.surface
-        )
-    }
-
-    Text(
-        text = "Creamy Donut",
-        style = MaterialTheme.typography.body1
-    )
 }
 
 @Composable
@@ -268,4 +184,59 @@ fun CuisineDetails() {
         style = MaterialTheme.typography.body1,
         textAlign = TextAlign.Center
     )
+}
+
+@Composable
+fun QuickRecipesWithHeader() {
+
+    Text(
+        text = stringResource(id = R.string.explore_quick_recipes_header),
+        style = MaterialTheme.typography.h6,
+    )
+
+    ScrollableRow(
+        horizontalArrangement = Arrangement.spacedBy(
+            space = 16.dp,
+            alignment = Alignment.Start
+        )
+    ) {
+
+        repeat(4) {
+            FoodCardWithDetails(
+                fraction = 0.8f,
+                imageResource = R.drawable.temp_noodles,
+                contentTags = "1 Serving • 20 Min • 205 Cal",
+                rating = "4.4",
+                name = "Asian Pesto Noodles",
+            )
+        }
+    }
+}
+
+
+@Composable
+fun RecentlyViewedRecipesWithHeader() {
+
+    Text(
+        text = stringResource(id = R.string.explore_quick_recipes_header),
+        style = MaterialTheme.typography.h6,
+    )
+
+    ScrollableRow(
+        horizontalArrangement = Arrangement.spacedBy(
+            space = 16.dp,
+            alignment = Alignment.Start
+        )
+    ) {
+
+        repeat(4) {
+            FoodCardWithDetails(
+                fraction = 0.8f,
+                imageResource = R.drawable.temp_pasta,
+                contentTags = "1 Serving • 25 Min • 190 Cal",
+                rating = "4.4",
+                name = "One Pot Pasta",
+            )
+        }
+    }
 }
---
+++ app/src/main/java/com/beetlestance/aphid/FoodCard.kt
@@ -0,0 +1,148 @@
+package com.beetlestance.aphid
+
+import androidx.annotation.DrawableRes
+import androidx.compose.foundation.Icon
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.Text
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.ExperimentalLayout
+import androidx.compose.foundation.layout.FlowCrossAxisAlignment
+import androidx.compose.foundation.layout.FlowMainAxisAlignment
+import androidx.compose.foundation.layout.FlowRow
+import androidx.compose.foundation.layout.SizeMode
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.preferredWidth
+import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.Card
+import androidx.compose.material.EmphasisAmbient
+import androidx.compose.material.IconToggleButton
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.ProvideEmphasis
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Shape
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.platform.ConfigurationAmbient
+import androidx.compose.ui.res.colorResource
+import androidx.compose.ui.res.imageResource
+import androidx.compose.ui.res.vectorResource
+import androidx.compose.ui.unit.Dp
+import androidx.compose.ui.unit.dp
+
+@Composable
+fun FoodCardWithDetails(
+    modifier: Modifier = Modifier,
+    fraction: Float = 1f,
+    verticalItemSpace: Dp = 4.dp,
+    @DrawableRes imageResource: Int,
+    cardShape: Shape = RoundedCornerShape(16.dp),
+    onCheckedChange: (Boolean) -> Unit = {},
+    contentTags: String,
+    rating: String,
+    name: String
+) {
+    // per item width in row
+    val itemWidth = (ConfigurationAmbient.current.screenWidthDp * fraction).dp
+
+    Column(
+        modifier = modifier.preferredWidth(itemWidth).fillMaxWidth(),
+        verticalArrangement = Arrangement.spacedBy(
+            space = verticalItemSpace,
+            alignment = Alignment.Top
+        )
+    ) {
+
+        FoodCardWithFavIcon(
+            imageResource = imageResource,
+            cardShape = cardShape,
+            onCheckChanged = onCheckedChange
+        )
+
+        FoodCardContentsDetails(
+            contentTags = contentTags,
+            rating = rating,
+            name = name
+        )
+    }
+}
+
+@Composable
+fun FoodCardWithFavIcon(
+    @DrawableRes imageResource: Int,
+    cardShape: Shape = RoundedCornerShape(16.dp),
+    onCheckChanged: (Boolean) -> Unit = {}
+) {
+    Card(shape = cardShape) {
+        Box {
+            Image(
+                modifier = Modifier.fillMaxWidth(),
+                asset = imageResource(id = imageResource),
+                contentScale = ContentScale.Crop
+            )
+
+            ProvideEmphasis(emphasis = EmphasisAmbient.current.medium) {
+                IconToggleButton(
+                    modifier = Modifier
+                        .align(Alignment.TopEnd)
+                        .padding(16.dp)
+                        .background(
+                            shape = CircleShape,
+                            color = colorResource(id = R.color.grey_400_alpha_30)
+                        ),
+                    icon = {
+                        Icon(
+                            asset = vectorResource(id = R.drawable.ic_like),
+                            tint = colorResource(R.color.deep_orange_a200)
+                        )
+                    },
+                    checked = false,
+                    onCheckedChange = onCheckChanged
+                )
+            }
+        }
+    }
+}
+
+@OptIn(ExperimentalLayout::class)
+@Composable
+fun FoodCardContentsDetails(
+    contentTags: String,
+    rating: String,
+    name: String
+) {
+    FlowRow(
+        mainAxisAlignment = FlowMainAxisAlignment.SpaceBetween,
+        mainAxisSize = SizeMode.Expand,
+        crossAxisAlignment = FlowCrossAxisAlignment.Center,
+        crossAxisSpacing = 4.dp
+    ) {
+        Text(
+            text = contentTags,
+            style = MaterialTheme.typography.body2,
+            color = colorResource(id = R.color.grey_700)
+        )
+
+        Text(
+            modifier = Modifier
+                .background(
+                    color = colorResource(id = R.color.deep_orange_a200),
+                    shape = RoundedCornerShape(10.dp)
+                )
+                .padding(horizontal = 12.dp, vertical = 2.dp),
+            text = rating,
+            style = MaterialTheme.typography.subtitle2,
+            color = MaterialTheme.colors.surface
+        )
+    }
+
+    Text(
+        text = name,
+        style = MaterialTheme.typography.body1
+    )
+}
