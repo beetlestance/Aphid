a6fe75fcc4d419adbb357051d52f3d4b96c7bd44
kameshyadavky <kameshyadavky@gmail.com>
2020-10-17 21:40:56 +0530

    Optimized imports
    Removed unnecessary imports

--- app/src/main/java/com/beetlestance/aphid/MainActivityViewModel.kt
+++ app/src/main/java/com/beetlestance/aphid/MainActivityViewModel.kt
@@ -1,19 +1,13 @@
 package com.beetlestance.aphid
 
 import androidx.hilt.lifecycle.ViewModelInject
-import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
 import com.beetlestance.base_android.AphidViewModel
 import com.beetlestance.domain.executors.FetchRecipes
-import kotlinx.coroutines.flow.firstOrNull
 import kotlinx.coroutines.launch
 import com.beetlestance.domain.invoke
 import com.beetlestance.domain.observers.ObserveRecipes
 import com.beetlestance.domain.watchStatus
-import com.beetlestance.spoonacular_kotlin.SpoonacularImageHelper
-import com.beetlestance.spoonacular_kotlin.constants.SpoonacularImageSize
-import kotlinx.coroutines.flow.collect
-import timber.log.Timber
 
 class MainActivityViewModel @ViewModelInject constructor(
     fetchRecipes: FetchRecipes,
