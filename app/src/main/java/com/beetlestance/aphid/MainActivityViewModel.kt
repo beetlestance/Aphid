d7ac5c8a8b729821504f8ecbf83b0987532f9d37
kameshyadavky <kameshyadavky@gmail.com>
2020-10-17 21:05:21 +0530

    ObserveRecipes.kt
    observe use case for Recipes

--- app/src/main/java/com/beetlestance/aphid/MainActivityViewModel.kt
+++ app/src/main/java/com/beetlestance/aphid/MainActivityViewModel.kt
@@ -8,40 +8,25 @@ import com.beetlestance.domain.executors.FetchRecipes
 import kotlinx.coroutines.flow.firstOrNull
 import kotlinx.coroutines.launch
 import com.beetlestance.domain.invoke
+import com.beetlestance.domain.observers.ObserveRecipes
 import com.beetlestance.spoonacular_kotlin.SpoonacularImageHelper
 import com.beetlestance.spoonacular_kotlin.constants.SpoonacularImageSize
+import kotlinx.coroutines.flow.collect
 import timber.log.Timber
 
 class MainActivityViewModel @ViewModelInject constructor(
-    fetchRecipes: FetchRecipes
+    fetchRecipes: FetchRecipes,
+    observeRecipes: ObserveRecipes
 ) : AphidViewModel<ExploreViewState>(ExploreViewState()) {
 
     init {
         viewModelScope.launch {
-            val recipes = fetchRecipes().firstOrNull() ?: return@launch
-            setState {
-                this.copy(breakfastRecipes = recipes.map { recipeInformation ->
-                    Recipe(
-                        id = recipeInformation.id,
-                        name = recipeInformation.title,
-                        image = SpoonacularImageHelper.generateRecipeImageUrl(
-                            id = recipeInformation.id?.toLong() ?: 0,
-                            imageSize = SpoonacularImageSize.Recipe.ULTRA_HIGH_QUALITY,
-                            imageType = recipeInformation.imageType ?: ""
-                        )
-                    )
-                })
-            }
+            fetchRecipes().firstOrNull() ?: return@launch
+        }
+
+        observeRecipes()
+        viewModelScope.launch {
+            observeRecipes.observe().collectAndSetState { copy(breakfastRecipes = it) }
         }
     }
 }
-
-data class Recipe(
-    val id: Int?,
-    val name: String? = null,
-    val rating: Int? = null,
-    val serving: Int? = null,
-    val time: Long? = null,
-    val calories: Long? = null,
-    val image: String? = null
-)
