18bfa46e883eb6a05bd029d0283372d42bed2532
kameshyadavky <kameshyadavky@gmail.com>
2020-11-07 21:53:09 +0530

    breakfast recipe worked

--- spoonacular-kotlin/src/main/java/com/beetlestance/spoonacular_kotlin/utils/Extensions.kt
+++ spoonacular-kotlin/src/main/java/com/beetlestance/spoonacular_kotlin/utils/Extensions.kt
@@ -1,21 +1,22 @@
 package com.beetlestance.spoonacular_kotlin.utils
 
-inline fun <T, reified R> T.serializedCopy(isNullSafe: Boolean = true): R? {
-
-    val adapter = MoshiSerializer.moshi.adapter(R::class.java)
-    if (isNullSafe) adapter.nullSafe()
-
-    return adapter.fromJsonValue(this)
+inline fun <reified T, reified R> T.serializedCopy(isNullSafe: Boolean = true): R? {
+    val sourceAdapter = MoshiSerializer.moshi.adapter(T::class.java)
+    if (isNullSafe) sourceAdapter.nullSafe()
+    val sourceJson = sourceAdapter.toJson(this)
+    val targetAdapter = MoshiSerializer.moshi.adapter(R::class.java)
+    if (isNullSafe) targetAdapter.nullSafe()
+    return targetAdapter.fromJson(sourceJson)
 }
 
-inline fun <T, reified R, C> T.serializedTransform(
+inline fun <reified T, reified R, C> T.serializedTransform(
     isNullSafe: Boolean = true,
     transform: (R) -> C
 ): C? {
     return transform(serializedCopy<T, R>(isNullSafe) ?: return null)
 }
 
-inline fun <T, reified R, C> Iterable<T>.serializedMapper(
+inline fun <reified T, C, reified R> Iterable<T>.serializedMapper(
     isNullSafe: Boolean = true,
     transform: (R) -> C
 ): List<C>? {
