c06b0bbd031fdab448313bb5b606d011c0f6a59f
akshay <akshay253101yadav@gmail.com>
2020-11-08 15:27:38 +0530

    type token added

--- spoonacular-kotlin/src/main/java/com/beetlestance/spoonacular_kotlin/models/response/recipe/SearchRecipeComplex.kt
+++ spoonacular-kotlin/src/main/java/com/beetlestance/spoonacular_kotlin/models/response/recipe/SearchRecipeComplex.kt
@@ -14,7 +14,7 @@ data class SearchRecipeComplex(
     val offset: Int? = null,
 
     @Json(name = "results")
-    val results: List<ResultsItem?>? = null
+    val results: List<ResultsItem>? = null
 ) {
 
     data class ResultsItem(
---
+++ spoonacular-kotlin/src/main/java/com/beetlestance/spoonacular_kotlin/utils/MoshiExtensions.kt
@@ -0,0 +1,65 @@
+package com.beetlestance.spoonacular_kotlin.utils
+
+import com.squareup.moshi.JsonAdapter
+import com.squareup.moshi.Moshi
+import com.squareup.moshi.Types
+import com.squareup.moshi.internal.Util
+import java.lang.reflect.ParameterizedType
+import java.lang.reflect.Type
+
+inline fun <reified T, reified R> T.serializedCopy(): R? {
+    val sourceJson = MoshiSerializer.moshi.toJson(this)
+    return MoshiSerializer.moshi.fromJson(sourceJson)
+}
+
+inline fun <reified T, reified R, C> T.serializedTransform(
+    transform: (R) -> C
+): C? {
+    return transform(serializedCopy<T, R>() ?: return null)
+}
+
+inline fun <reified T, C, reified R> Iterable<T>.serializedMapper(
+    transform: (R) -> C
+): List<C>? {
+    return mapNotNull { data ->
+        val result: R = data.serializedCopy() ?: return@mapNotNull null
+        transform(result)
+    }
+}
+
+open class TypeToken<T> {
+    /**
+     * Gets the generic super class type.
+     */
+    val type: Type = superClassTypeParameter()
+    fun superClassTypeParameter(): Type {
+        val superClass = javaClass.genericSuperclass
+        if (superClass is Class<*>) {
+            throw RuntimeException("Missing type parameter.")
+        }
+        // This is to get [T] from TypeToken
+        val parameterType = (superClass as ParameterizedType).actualTypeArguments[0]
+        val argumentType = if (parameterType is ParameterizedType) {
+            // find argument type is [T] is parameterized
+            parameterType.actualTypeArguments.first()
+        } else parameterType
+        return Util.canonicalize(argumentType)
+    }
+}
+
+inline fun <reified T> Moshi.adapter(): JsonAdapter<T> {
+    val type = object : TypeToken<T>() {}.type
+    val adapterType = Types.newParameterizedType(T::class.java, type)
+    val adapter: JsonAdapter<T> = adapter(adapterType)
+    return adapter.nullSafe()
+}
+
+inline fun <reified T> Moshi.toJson(source: T): String {
+    val adapter: JsonAdapter<T> = adapter()
+    return adapter.toJson(source)
+}
+
+inline fun <reified T> Moshi.fromJson(source: String): T? {
+    val adapter: JsonAdapter<T> = adapter()
+    return adapter.fromJson(source)
+}
