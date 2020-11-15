5d74e62416486aea4d123326f2255c5c0bb917a9
kameshyadavky <kameshyadavky@gmail.com>
2020-11-15 18:52:23 +0530

    optimized imports

--- feature-explore/src/main/java/com/beetlestance/aphid/feature_explore/ExploreSearch.kt
+++ feature-explore/src/main/java/com/beetlestance/aphid/feature_explore/ExploreSearch.kt
@@ -4,7 +4,6 @@ import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.shape.CircleShape
-import androidx.compose.material.BottomNavigation
 import androidx.compose.material.Icon
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Text
@@ -18,7 +17,6 @@ import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
 import androidx.compose.ui.draw.drawShadow
-import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.text.input.TextFieldValue
 import androidx.compose.ui.unit.dp
 import androidx.ui.tooling.preview.Preview
