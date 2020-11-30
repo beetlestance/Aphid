b67e4a29cc791025c98d11be1f235a9c77d83284
kameshyadavky <kameshyadavky@gmail.com>
2020-11-30 11:23:08 +0530

    Created AphidContentSetup.kt

--- common-compose/src/main/java/com/beetlestance/aphid/common_compose/AphidContentSetup.kt
+++ common-compose/src/main/java/com/beetlestance/aphid/common_compose/AphidContentSetup.kt
@@ -2,11 +2,31 @@
 
 package com.beetlestance.aphid.common_compose
 
+import androidx.activity.ComponentActivity
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.platform.setContent
+import androidx.hilt.lifecycle.HiltViewModelFactory
+import com.beetlestance.aphid.common_compose.utils.ProvideNavigationViewModelFactoryMap
+import com.google.android.material.composethemeadapter.MdcTheme
+import dev.chrisbanes.accompanist.insets.ProvideWindowInsets
 
 @Composable
-inline fun AphidContentSetUp(
-    noinline content: @Composable () -> Unit
+fun ComponentActivity.AphidContentSetUp(
+    content: @Composable () -> Unit
 ) {
-
+    setContent {
+        // Provided mdc theme
+        MdcTheme {
+            // Provides window insets
+            ProvideWindowInsets {
+                // Provides view model
+                ProvideNavigationViewModelFactoryMap(
+                    factory = defaultViewModelProviderFactory as HiltViewModelFactory
+                ) {
+                    // actual content
+                    content()
+                }
+            }
+        }
+    }
 }
