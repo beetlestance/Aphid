7f9a18c122e7aac3ae547e00d2faadea187f8526
akshay yadav <akshay253101yadav@gmail.com>
2021-01-02 10:28:56 +0530

    wild card imports removed

--- feature-chat/src/test/java/com/beetlestance/aphid/feature_chat/ExampleUnitTest.kt
+++ feature-chat/src/test/java/com/beetlestance/aphid/feature_chat/ExampleUnitTest.kt
@@ -1,8 +1,7 @@
 package com.beetlestance.aphid.feature_chat
 
 import org.junit.Test
-
-import org.junit.Assert.*
+import org.junit.Assert.assertEquals
 
 /**
  * Example local unit test, which will execute on the development machine (host).
