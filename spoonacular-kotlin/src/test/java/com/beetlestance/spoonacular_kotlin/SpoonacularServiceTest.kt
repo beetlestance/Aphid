9b83389d650aec1432c5fd7359fee863f0358867
akshay yadav <akshay253101yadav@gmail.com>
2021-01-03 18:09:11 +0530

    api key test validation added

--- spoonacular-kotlin/src/test/java/com/beetlestance/spoonacular_kotlin/SpoonacularServiceTest.kt
+++ spoonacular-kotlin/src/test/java/com/beetlestance/spoonacular_kotlin/SpoonacularServiceTest.kt
@@ -2,6 +2,7 @@ package com.beetlestance.spoonacular_kotlin
 
 import com.beetlestance.spoonacular_kotlin.services.RecipesService
 import com.google.common.truth.Truth.assertThat
+import com.google.common.truth.Truth.assertWithMessage
 import org.junit.Before
 import org.junit.Test
 
@@ -20,9 +21,12 @@ class SpoonacularServiceTest {
 
     @Test
     fun validateApiKey() {
-        val api = System.getenv()["TEST_API_KEY"]
-        assertThat(api.isNullOrBlank().not())
-        print(api)
+        val api = System.getenv().getOrDefault(API_KEY_PARAM, "")
+        assertWithMessage("No API Key provided").that(api).isNotEmpty()
+    }
+
+    companion object {
+        val API_KEY_PARAM = "TEST_API_KEY"
     }
 
 }
