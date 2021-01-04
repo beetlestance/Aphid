3240e2204c87d7e4ab9b735779e3e76b47bf2b9d
akshay yadav <akshay253101yadav@gmail.com>
2021-01-04 09:44:22 +0530

    removed redundant beforeClass

--- spoonacular-kotlin/src/test/java/com/beetlestance/spoonacular_kotlin/SpoonacularTest.kt
+++ spoonacular-kotlin/src/test/java/com/beetlestance/spoonacular_kotlin/SpoonacularTest.kt
@@ -2,16 +2,22 @@ package com.beetlestance.spoonacular_kotlin
 
 import com.google.common.truth.Truth
 import okhttp3.OkHttpClient
-import org.junit.Before
-import org.junit.BeforeClass
 import org.junit.Test
-import org.junit.runner.RunWith
-import java.util.concurrent.TimeUnit
 import java.util.concurrent.TimeUnit.SECONDS
 
 open class SpoonacularTest {
 
-    val spoonacular: Spoonacular = provideSpoonacular()
+    private val apiKey = System.getenv().getOrDefault(API_KEY_PARAM, "")
+
+    val spoonacular: Spoonacular = object : Spoonacular(apiKey) {
+        override fun okHttpClient(): OkHttpClient {
+            return okHttpClientBuilder().apply {
+                connectTimeout(20, SECONDS)
+                readTimeout(20, SECONDS)
+                writeTimeout(20, SECONDS)
+            }.build()
+        }
+    }
 
     @Test
     fun validateApiKey() = Truth.assertWithMessage("No API Key provided").that(apiKey).isNotEmpty()
@@ -20,20 +26,6 @@ open class SpoonacularTest {
 
         private const val API_KEY_PARAM = "TEST_API_KEY"
 
-        private val apiKey = System.getenv().getOrDefault(API_KEY_PARAM, "")
-
-        @BeforeClass
-        private fun provideSpoonacular(): Spoonacular {
-            return object : Spoonacular(apiKey) {
-                override fun okHttpClient(): OkHttpClient {
-                    return okHttpClientBuilder().apply {
-                        connectTimeout(20, SECONDS)
-                        readTimeout(20, SECONDS)
-                        writeTimeout(20, SECONDS)
-                    }.build()
-                }
-            }
-        }
     }
 
 }
--- spoonacular-kotlin/src/test/java/com/beetlestance/spoonacular_kotlin/services/FoodTest.kt
+++ spoonacular-kotlin/src/test/java/com/beetlestance/spoonacular_kotlin/services/FoodTest.kt
@@ -1,11 +1,6 @@
 package com.beetlestance.spoonacular_kotlin.services
 
 import com.beetlestance.spoonacular_kotlin.SpoonacularTest
-import com.google.common.truth.Truth
-import com.google.common.truth.TruthFailureSubject
-import com.google.common.truth.TruthJUnit
-import junit.framework.Assert.fail
-import org.junit.Test
 
 class FoodTest : SpoonacularTest() {
 
