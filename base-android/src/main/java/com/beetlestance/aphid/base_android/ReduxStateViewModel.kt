c4e964b9828639b32ee7be01c5f0ed3ea583be4e
Akshay Yadav <akshay253101yadav@gmail.com>
2022-01-23 23:07:33 +0530

    [Refactor] remove unused api's from redux state view model

--- base-android/src/main/java/com/beetlestance/aphid/base_android/ReduxStateViewModel.kt
+++ base-android/src/main/java/com/beetlestance/aphid/base_android/ReduxStateViewModel.kt
@@ -16,8 +16,6 @@
 package com.beetlestance.aphid.base_android
 
 import androidx.lifecycle.ViewModel
-import androidx.lifecycle.viewModelScope
-import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.flow.Flow
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.StateFlow
@@ -25,85 +23,28 @@ import kotlinx.coroutines.flow.asStateFlow
 import kotlinx.coroutines.flow.collect
 import kotlinx.coroutines.flow.distinctUntilChanged
 import kotlinx.coroutines.flow.map
-import kotlinx.coroutines.launch
 import kotlinx.coroutines.sync.Mutex
 import kotlinx.coroutines.sync.withLock
 import kotlin.reflect.KProperty1
 
-abstract class ReduxStateViewModel<S>(
-    initialState: S
-) : ViewModel() {
+abstract class ReduxStateViewModel<S> constructor(initialState: S) : ViewModel() {
     private val _state = MutableStateFlow(initialState)
     private val stateMutex = Mutex()
 
-    /**
-     * Returns a snapshot of the current state.
-     */
     val state: StateFlow<S>
         get() = _state.asStateFlow()
 
-    /**
-     * Extension function to collect and set state
-     */
-    protected suspend inline fun <reified T> Flow<T>.collectAndSetState(
-        crossinline reducer: suspend S.(T) -> S
-    ) = collect { item -> setState { reducer(item) } }
-
-    fun <A> selectObserve(prop1: KProperty1<S, A>): Flow<A> {
-        return selectSubscribe(prop1)
+    fun <A> KProperty1<S, A>.asFlow(): Flow<A> {
+        return _state.map { get(it) }.distinctUntilChanged()
     }
 
-    /**
-     * Subscribe to state and perform [block]
-     */
-    protected fun subscribe(block: (S) -> Unit) {
-        viewModelScope.launch {
-            _state.collect { block(it) }
-        }
-    }
-
-    /**
-     * Subscribe to a property in state for changes and performs [block]
-     */
-    protected fun <A> selectSubscribe(prop1: KProperty1<S, A>, block: (A) -> Unit) {
-        viewModelScope.launch {
-            selectSubscribe(prop1).collect { block(it) }
-        }
-    }
-
-    private fun <A> selectSubscribe(prop1: KProperty1<S, A>): Flow<A> {
-        return _state.map { prop1.get(it) }.distinctUntilChanged()
-    }
-
-    /**
-     * Updates the state of view model
-     */
     protected suspend fun setState(reducer: suspend S.() -> S) {
         stateMutex.withLock {
             _state.value = reducer(_state.value)
         }
     }
 
-    /**
-     * Provide a launch block with state update
-     */
-    protected fun CoroutineScope.launchSetState(reducer: suspend S.() -> S) {
-        launch { this@ReduxStateViewModel.setState(reducer) }
-    }
-
-    /**
-     * Perform [block] with current state as parameter
-     */
-    protected suspend fun withState(block: (S) -> Unit) {
-        stateMutex.withLock {
-            block(_state.value)
-        }
-    }
-
-    /**
-     * Scope extension to perform [block] on current state
-     */
-    protected fun CoroutineScope.withState(block: (S) -> Unit) {
-        launch { this@ReduxStateViewModel.withState(block) }
-    }
+    protected suspend inline fun <reified T> Flow<T>.collectAndSetState(
+        crossinline reducer: suspend S.(T) -> S
+    ) = collect { item -> setState { reducer(item) } }
 }
