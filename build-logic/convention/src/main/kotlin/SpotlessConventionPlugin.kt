bd783403e4b56b59e28616a5019e4a8ac3ab80f7
kameshyadavky <kameshyadavky@gmail.com>
2022-07-25 19:38:31 +0530

    [Fix] removed spotless extra configuration
    Will add later if necessary

--- build-logic/convention/src/main/kotlin/SpotlessConventionPlugin.kt
+++ build-logic/convention/src/main/kotlin/SpotlessConventionPlugin.kt
@@ -18,18 +18,6 @@ class SpotlessConventionPlugin : Plugin<Project> {
                     ktlint(libs.findVersion("ktlint").get().toString()).userData(mapOf("android" to "true"))
                     licenseHeaderFile(rootProject.file("spotless/copyright.kt"))
                 }
-                format("kts") {
-                    target("**/*.kts")
-                    targetExclude("**/build/**/*.kts")
-                    // Look for the first line that doesn't have a block comment (assumed to be the license)
-                    licenseHeaderFile(rootProject.file("spotless/copyright.kts"), "(^(?![\\/ ]\\*).*$)")
-                }
-                format("xml") {
-                    target("**/*.xml")
-                    targetExclude("**/build/**/*.xml")
-                    // Look for the first XML tag that isn't a comment (<!--) or the xml declaration (<?xml)
-                    licenseHeaderFile(rootProject.file("spotless/copyright.xml"), "(<[^!?])")
-                }
             }
         }
     }
